aa2c021afcfa4a0b3550124014570fa1e3a5bc6c
Stephen <stephen@whitebolt.net>
2018-05-31 11:26:59 +0100

    Change to use @simpo module.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import TinyMCE from 'react-tinymce';
+import TinyMCE from '@simpo/react-tinymce';
 
 const TinyMCEInput = ({
   id,
